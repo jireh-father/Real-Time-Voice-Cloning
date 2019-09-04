cb56f1656a289a5c2181d7ac0fa352731dbd1974
seoilgun <seoilgun@gmail.com>
2019-09-04 11:27:26 +0900

    전처리추가

--- vocoder/inference.py
+++ vocoder/inference.py
@@ -51,8 +51,8 @@ def infer_waveform(mel, normalize=True,  batched=True, target=8000, overlap=800,
     if _model is None:
         raise Exception("Please load Wave-RNN in memory before using it")
     
-    if normalize:
-        mel = mel / hp.mel_max_abs_value
-    mel = torch.from_numpy(mel[None, ...])
+    # if normalize:
+    #     mel = mel / hp.mel_max_abs_value
+    # mel = torch.from_numpy(mel[None, ...])
     wav = _model.generate(mel, batched, target, overlap, hp.mu_law, progress_callback)
     return wav
